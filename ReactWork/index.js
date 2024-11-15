2f06f891654b7f937a79c97c5f43dc0b5d6c2e3d
DurgeshSChauhan007 <131304057+DurgeshSChauhan007@users.noreply.github.com>
2024-11-15 21:20:44 +0530

    Add files via upload

---
+++ ReactWork/index.js
@@ -0,0 +1,12 @@
+// export let myName = "Rahul";
+// export function hi() {
+//     return "Hi, working on ES6 modules";
+// }
+
+// export let calc = Math.random()*1000;
+
+function msg() {
+    return Math.random() * 1000;
+}
+
+export default msg;
